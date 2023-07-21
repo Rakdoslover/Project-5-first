25089c8989e0247700b6296d3663ab172fbe5a18
Rakdoslover <>
2023-07-21 13:59:15 +0000

    made changes to my instructors.html files to integrate info on my instructors and some pictures, having a problem with getting the pictures to load properly

--- events/views.py
+++ events/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic, View
 from .models import Event
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.models import User
-from django.views.generic.edit import DeleteView, UpdateView
 from django.urls import reverse_lazy
 
-from django.contrib.auth.mixins import (
-    LoginRequiredMixin
-)
+
+def events(request):
+    """ A view to return the events page """
+
+    events = Event.objects.all()
+
+    context = {
+        'events': events
+    }
+
+    return render(request, 'events/events.html', context)
