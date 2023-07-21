6bcd78d4d36f714f349246a8230477f658c67390
Rakdoslover <>
2023-07-21 10:53:00 +0000

    costumized the admin layout for my instructors

--- instructors/admin.py
+++ instructors/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from .models import Instructor
 
 # Register your models here.
-admin.site.register(Instructor)
+class InstructorAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'sessions',
+        'years_of_experience',
+        'words_from_the_instructor',
+        'image_url',
+        'image',
+    )
+
+    ordering = ('name',)
+
+admin.site.register(Instructor, InstructorAdmin)
