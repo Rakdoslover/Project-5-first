cd10501e31dd28650dd091da40cab4b41d41b38a
Rakdoslover <>
2023-07-22 15:25:44 +0000

    Trying install the our_sessions.json but having som problem with the foreignkeys for my instructors

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Workout_Session
 
-# Register your models here.
+
+admin.site.register(Workout_Session)
--- workouts/models.py
+++ workouts/models.py
@@ -5,6 +5,7 @@ from instructors.models import Instructor
 class Workout_Session(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
+    place = models.TextField()
     instructors = models.ForeignKey(Instructor, on_delete=models.CASCADE)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
