a14896e688eaac3058b416dd413a5c009e1b03f7
Rakdoslover <>
2023-07-24 14:23:00 +0000

    got the increment button to add a session to my bag, but the decrement button does the same. Stille won't open the bag window either

--- bag/views.py
+++ bag/views.py
@@ -18,7 +18,7 @@ def add_to_bag(request, item_id):
     session = get_object_or_404(Workout_Session, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    size = None
+    bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
