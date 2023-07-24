dfd61a57223ebafac67a709b54063ac549b39ace
Rakdoslover <>
2023-07-24 11:32:28 +0000

    first deploy to heroku, changed the Debug in settings to make it switch between development stages

--- workouts/views.py
+++ workouts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from django.views import generic, View
 from .models import Workout_Session
 
@@ -13,3 +14,16 @@ def workouts(request):
     }
 
     return render(request, 'our-sessions/our-sessions.html', context)
+
+
+def session_detail(request, workout_session_id):
+    """ A view to show individual session """
+
+    workout_sessions = get_object_or_404(
+        Workout_Session, pk=workout_session_id)
+
+    context = {
+        'workout_sessions': workout_sessions
+    }
+
+    return render(request, 'workouts/session-detail.html', context)
