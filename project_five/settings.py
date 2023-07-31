b66b40e6c1d0c0d884da448374ae596c751013c2
Rakdoslover <>
2023-07-31 10:19:15 +0000

    switched back to original

--- project_five/settings.py
+++ project_five/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False # 'DEVELOPMENT' in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-rakdoslover-project-5-fi-zy2blpdei7.us2.codeanyapp.com',
