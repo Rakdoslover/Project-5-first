5a056005463c8191b0f91f5a6468a4e90dca5b39
Oscar Lundsör <oscarlundsor@gmail.com>
2023-08-23 11:35:39 +0000

    a save before tutor sessions, changes to the instructor.html to get the CSS to show up

--- project_five/urls.py
+++ project_five/urls.py
@@ -30,4 +30,4 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('summernote/', include('django_summernote.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
